6577157040de7a6c8208756f5af8caf8ed582a07
rashedevs <rasheduap2015@gmail.com>
2022-05-05 17:00:43 +0600

    custom link implemented on navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="success" variant="dark" className='navbar'>
   <Container>
   <Navbar.Brand as={Link} to='/' style={{fontSize:"23px"}}>Carventory</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -13,10 +14,10 @@ const Header = () => {
     <Nav className="me-auto">
     </Nav>
     <Nav className='nav-items'>
-      <Nav.Link to='/'>Home</Nav.Link>
-      <Nav.Link to='/manage'>Manage Items</Nav.Link>
-      <Nav.Link to='/add'>Add Item</Nav.Link>
-      <Nav.Link to='/blogs'>Blogs</Nav.Link>
+      <CustomLink to='/'>Home</CustomLink>
+      <CustomLink to='/manage'>Manage Items</CustomLink>
+      <CustomLink to='/add'>Add Item</CustomLink>
+      <CustomLink to='/blogs'>Blogs</CustomLink>
     </Nav>
   </Navbar.Collapse>
   </Container>
