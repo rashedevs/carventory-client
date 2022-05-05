f2dcff584f188f2c61b2eeb872b48851a7d6981d
rashedevs <rasheduap2015@gmail.com>
2022-05-05 20:13:09 +0600

    banner carousel added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h3>This is blogs</h3>
+        <div className='container'>
+            <div className='w-50 mx-auto'>
+                <h4>This is question one</h4>
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae quas harum! Officiis quibusdam tempora nostrum assumenda, ab amet voluptatum necessitatibus voluptatem nihil fugiat non, vitae nulla earum minima consequatur neque, veritatis nam! Iste quidem, asperiores id error ex sapiente sed nemo est repellendus quibusdam eaque quo odit amet unde?</p>
+            </div>
+            <div className='w-50 mx-auto'>
+                <h4>This is question one</h4>
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae quas harum! Officiis quibusdam tempora nostrum assumenda, ab amet voluptatum necessitatibus voluptatem nihil fugiat non, vitae nulla earum minima consequatur neque, veritatis nam! Iste quidem, asperiores id error ex sapiente sed nemo est repellendus quibusdam eaque quo odit amet unde?</p>
+            </div>
+            <div className='w-50 mx-auto'>
+                <h4>This is question one</h4>
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae quas harum! Officiis quibusdam tempora nostrum assumenda, ab amet voluptatum necessitatibus voluptatem nihil fugiat non, vitae nulla earum minima consequatur neque, veritatis nam! Iste quidem, asperiores id error ex sapiente sed nemo est repellendus quibusdam eaque quo odit amet unde?</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
     return (
         <div>
-            <p>This is banner</p>
+            <Carousel fade>
+  <Carousel.Item>
+    <img style={{ height: "550px" }}
+      className="d-block w-100"
+      src="https://cdn.pixabay.com/photo/2015/10/01/17/17/car-967387__480.png"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img style={{ height: "550px" }}
+      className="d-block w-100"
+      src="https://freepngimg.com/download/bmw/60179-series-x3-sedan-bmw-car-free-photo-png.png"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img style={{ height: "550px" }}
+      className="d-block w-100"
+      src="https://pngimg.com/uploads/mercedes/mercedes_PNG80135.png"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
         </div>
     );
 };
