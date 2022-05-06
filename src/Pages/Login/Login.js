81664f572359daed29cc13062d03dfb563498a53
rashedevs <rasheduap2015@gmail.com>
2022-05-06 06:58:49 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h3>This is login form</h3>
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-info my-5 text-center'>Please Login</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control  type="email" placeholder="Enter Email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+
+                    <Form.Control type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="info" className='mx-auto w-50 my-2 fw-bold text-white' type="submit">
+                    Login
+                </Button>
+            </Form>
         </div>
     );
 };
