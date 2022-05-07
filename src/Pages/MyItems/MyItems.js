3611845aa674697143cafdb4e6ee55f418dc104c
rashedevs <rasheduap2015@gmail.com>
2022-05-07 18:54:16 +0600

    delete method added on my item page

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import "./MyItems.css";
 
 const MyItems = () => {
   const [user] = useAuthState(auth);
@@ -17,9 +19,52 @@ const MyItems = () => {
     };
     getMyItems();
   }, [user]);
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/items/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remaining = myItems.filter((item) => item._id !== id);
+          setmyItems(remaining);
+        });
+    }
+  };
   return (
-    <div className="container" style={{ height: "100vh" }}>
-      <h3>My items: {myItems.length}</h3>
+    <div className="my-4">
+      <h3 className="my-2">My items</h3>
+      <hr className="w-50 text-danger mx-auto mb-4" />
+      <div className=" container items-container">
+        {myItems.map((item) => (
+          <div key={item._id}>
+            <Card className="text-center mx-auto h-100">
+              <Card.Img
+                variant="top"
+                className="rounded"
+                style={{ width: "100%", height: "250px" }}
+                src={item.img}
+              />
+
+              <Card.Body>
+                <Card.Title>Name: {item.name}</Card.Title>
+                <Card.Text>Description: {item.description}</Card.Text>
+                <Card.Text>Supplier: {item.supplier}</Card.Text>
+                <Card.Text>Quantity: {item.quantity}</Card.Text>
+                <Card.Text>Price: ${item.price}</Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => handleDelete(item._id)}
+                >
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
