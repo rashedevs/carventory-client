b407c8c85e400853c690d7890d21d3a6f833c7d9
rashedevs <rasheduap2015@gmail.com>
2022-05-07 08:42:45 +0600

    inventory items loaded from db to the ui

--- src/Pages/Home/Items/Items.js
+++ src/Pages/Home/Items/Items.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Item from "../../Item/Item";
+import "./Items.css";
 
 const Items = () => {
-    return (
-        <div>
-            <p>6 sliced Items</p>
-            <Link to="/inventory/:id">
-     <button type="button">
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/product")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="container">
+      <h4 className="my-5">Six sliced item</h4>
+      <div className="product-container">
+        {products.slice(0, 6).map((product) => (
+          <Item key={product._id} product={product}></Item>
+        ))}
+      </div>
+      <Link to="/manageitems">
+        <button className="my-3" type="button">
           Manage Inventories
-     </button>
- </Link>
-        </div>
-    );
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Items;
---
+++ src/Pages/Item/Item.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const Item = ({ product }) => {
+  const { _id, name, price, description, img, quantity, supplier } = product;
+  const navigate = useNavigate();
+  const navigateToInventory = (id) => {
+    navigate(`/inventory/${id}`);
+  };
+  return (
+    <div>
+      <Card style={{ height: "31rem" }}>
+        <Card.Img variant="top" style={{ height: "180px" }} src={img} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Supplier: {supplier}</ListGroupItem>
+          <ListGroupItem>Quantity: {quantity}</ListGroupItem>
+          <ListGroupItem>Price: ${price}</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link>
+            <button
+              className="btn-primary"
+              onClick={() => navigateToInventory(_id)}
+            >
+              Stock Update
+            </button>
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Item;
