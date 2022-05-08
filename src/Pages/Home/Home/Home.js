dc4926afe3121019979324bbdf3b36b19a819839
rashedevs <rasheduap2015@gmail.com>
2022-05-08 16:58:16 +0600

    warehouse page added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Items from '../Items/Items';
+import React from "react";
+import Warhouses from "../../Warehouses/Warehouses";
+import Banner from "../Banner/Banner";
+import Items from "../Items/Items";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="banner">
-                <Banner></Banner>
-            </div>
-            <div className="items">
-                <Items></Items>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="banner">
+        <Banner></Banner>
+      </div>
+      <div className="items">
+        <Items></Items>
+      </div>
+      <div>
+        <Warhouses></Warhouses>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Warehouses/Warehouses.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Warehouses.css";
+import warehouse1 from "../../images/warehouses/warehouse1.jpg";
+import warehouse2 from "../../images/warehouses/warehouse2.jpg";
+import warehouse3 from "../../images/warehouses/warehouse3.jpg";
+
+const Warhouses = () => {
+  return (
+    <div className="mb-5">
+      <h4 className=" text-danger">Our Warehouses</h4>
+      <hr className="w-50 mx-auto text-danger mb-4" />
+      <div className="warehouse container w-75 mx-auto">
+        <div>
+          <img
+            style={{ height: "200px", width: "350px" }}
+            src={warehouse2}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "200px", width: "350px" }}
+            src={warehouse1}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "200px", width: "350px" }}
+            src={warehouse3}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Warhouses;
